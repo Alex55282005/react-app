333a49535cfbcf80727fda9454ac64db9f439741
Alex55282005 <oleg.shavchenko.2005@gmail.com>
2022-07-26 21:14:06 +0300

    dz10.1

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,37 +1,29 @@
 import { Component } from "react";
-export default class TodoItem extends Component {
-  constructor() {
-    super();
-    console.log("TODO ITEM CONSTRUCTOR");
-  }
-  render() {
-    const { completed, title, id } = this.props.item;
-    return (
-      <li
-        style={{ backgroundColor: getStyle(completed), height: "30px", marginTop: "10px", borderRadius: "10px" }}
-        onClick={() => this.props.onItemClick(id)}
+
+
+export default function TodoItem({onItemClick,onDeleteButtonClick, item}) {
+  const { completed, title, id } = item;
+  return (
+    <li
+      style={{ 
+        backgroundColor: getStyle(completed), 
+        height: "30px", 
+        marginTop: "10px", 
+        borderRadius: "10px" }}
+      onClick={() => onItemClick(id)}
+    >
+      {title}
+      <button
+        onClick={(e) => {
+          e.stopPropagation();
+          onDeleteButtonClick(id);
+        }}
+        title="Delete item"
       >
-        {title}
-        <button
-          onClick={(e) => {
-            e.stopPropagation();
-            this.props.onDeleteButtonClick(id);
-          }}
-          title="Delete item"
-        >
-          Delete
-        </button>
-      </li>
-    );
-  }
-
-  componentDidMount() {
-    console.log("TODO ITEM DID MOUNT");
-  }
-
-  componentWillUnmount() {
-    console.log("TODO ITEM DID UNMOUNT");
-  }
+        Delete
+      </button>
+    </li>
+  );
 }
 
 function getStyle(completed) {
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,92 +1,84 @@
-import { Component } from "react";
+import { Component, useEffect, useState } from "react";
 import TodoItem from "./TodoItem";
-export default class TodosList extends Component {
-  state = {
-    todos: [],
-    title: "",
-  };
-  constructor() {
-    super();
-    this.onDeleteButtonClick = this.onDeleteButtonClick.bind(this);
-    this.onItemClick = this.onItemClick.bind(this);
-    this.onSubmitButtonClick = this.onSubmitButtonClick.bind(this);
-    this.onInputChange = this.onInputChange.bind(this);
-    console.log("TODO LIST CONSTRUCTOR");
+
+import React from "react";
+
+const axios = require("axios").default;
+
+export default function TodoList() {
+  const [todos, setTodos] = useState([]);
+  const [title, setTitle] = useState([]);
+
+
+  useEffect(() => {
+      axios.get("https://612687da3ab4100017a68fd8.mockapi.io/todos")
+        .then((resp) => setTodos(resp.data));
+  }, [])
+
+  
+
+  const onInputChange = (e) => {
+    setTitle(e.target.value );
   }
-  render() {
-    return (
-      <>
-        <ul>
-          {this.state.todos.map((todo) => (
-            <TodoItem
-              key={todo.id}
-              item={todo}
-              onDeleteButtonClick={this.onDeleteButtonClick}
-              onItemClick={this.onItemClick}
-            />
-          ))}
-        </ul>
-        <form action="">
-          <fieldset>
-            <legend>Enter new todo</legend>
-            <input
-              type="text"
-              value={this.state.title}
-              onChange={this.onInputChange}
-            />
-            <button onClick={this.onSubmitButtonClick}>Add</button>
-          </fieldset>
-        </form>
-      </>
+
+  const onSubmitButtonClick =(e) =>{
+    e.preventDefault();
+    const newItem = {
+      title: title,
+      completed: false,
+    };
+    axios.post("https://612687da3ab4100017a68fd8.mockapi.io/todos", newItem
+    ).then(({data}) =>
+    setTodos((prevTodos) => [...prevTodos, data])
+    );
+      
+  }
+
+  const onItemClick =(id) =>{
+    const item = todos.find((todo) => todo.id === id);
+    const newItem = { ...item, completed: !item.completed };
+    axios.put("https://612687da3ab4100017a68fd8.mockapi.io/todos" + "/" + id, newItem);
+    setTodos(
+      todos.map((item) => (item.id === id ? newItem : item)),
     );
   }
 
-  onInputChange(e) {
-    this.setState({ title: e.target.value });
-  }
-
-  onSubmitButtonClick(e) {
-    e.preventDefault();
-    const newItem = {
-      title: this.state.title,
-      completed: false,
-    };
-    fetch("https://612687da3ab4100017a68fd8.mockapi.io/todos", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(newItem),
-    })
-      .then((resp) => resp.json())
-      .then((data) =>
-        this.setState({ todos: [...this.state.todos, data], title: "" })
-      );
-  }
-
-  onItemClick(id) {
-    const item = this.state.todos.find((todo) => todo.id === id);
-    const newItem = { ...item, completed: !item.completed };
-    fetch("https://612687da3ab4100017a68fd8.mockapi.io/todos" + "/" + id, {
-      method: "PUT",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(newItem),
-    });
-    this.setState({
-      todos: this.state.todos.map((item) => (item.id === id ? newItem : item)),
-    });
-  }
-
-  onDeleteButtonClick(id) {
+  const onDeleteButtonClick = (id) =>{
     fetch("https://612687da3ab4100017a68fd8.mockapi.io/todos" + "/" + id, {
       method: "DELETE",
     });
-    const newTodos = this.state.todos.filter((todo) => todo.id !== id);
-    this.setState({ todos: newTodos });
+    const newTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(newTodos);
   }
 
-  componentDidMount() {
-    console.log("TODO LIST DID MOUNT");
-    fetch("https://612687da3ab4100017a68fd8.mockapi.io/todos")
-      .then((resp) => resp.json())
-      .then((data) => this.setState({ todos: data }));
-  }
+  
+
+  return (
+    <>
+      <ul>
+        {todos.map((todo) => (
+          <TodoItem
+            key={todo.id}
+            item={todo}
+            onDeleteButtonClick={onDeleteButtonClick}
+            onItemClick={onItemClick}
+          />
+        ))}
+      </ul>
+      <form action="">
+        <fieldset>
+          <legend>Enter new todo</legend>
+          <input
+            type="text"
+            value={title}
+            onChange={onInputChange}
+          />
+          <button onClick={onSubmitButtonClick}>Add</button>
+        </fieldset>
+      </form>
+    </>
+  );
 }
+
+
+
